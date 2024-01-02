a594d7e84b517086607f16e7bc3c5918e01b70f6
Shikharawat2002 <rawatsikha112@gmail.com>
2024-01-02 15:16:36 +0530

    fetch user message id

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.querySelector('button').addEventListener('click', function () {
     // Open the interactive OAuth consent screen
     console.log("Consent screen starting");
-    chrome.identity.getAuthToken({ interactive: true }, function (token) {
+    chrome.identity.getAuthToken({ interactive: true }, async function (token) {
         if (chrome.runtime.lastError || !token) {
             // Handle errors or user rejection
             console.error(chrome.runtime.lastError);
@@ -11,21 +11,26 @@ document.querySelector('button').addEventListener('click', function () {
         console.log("Consent screen end");
 
         // Call the function with the obtained token
-        makeApiRequest(token);
+        await makeApiRequest(token);
     });
-});
-
-let threadId;
-let messageId;
-
-document.getElementById('thread').addEventListener('click', function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.scripting.executeScript({
+    
+    chrome.tabs.query({ active: true, currentWindow: true },async function (tabs) {
+       await chrome.scripting.executeScript({
             target: { tabId: tabs[0].id },
             function: getThread,
         });
     });
+
+      //email id
+      chrome.identity.getProfileUserInfo(async function(info) {  email = info.email;
+        await console.log("email::", email) });
+
 });
+
+
+// document.getElementById('thread').addEventListener('click', function () {
+    
+// });
 
 async function makeApiRequest(token) {
     // Use the obtained token and threadId to make authorized API requests
@@ -39,31 +44,54 @@ async function makeApiRequest(token) {
         'contentType': 'json'
     };
 
-    fetch(
-        `https://gmail.googleapis.com/gmail/v1/users/shikhatesting0@gmail.com/threads`,
-        init
-    )
-        .then((response) => response.json())
-        .then(function (data) {
-            console.log(data);
-        })
-        .catch(function (error) {
-            console.error('API request error:', error);
-        });
+    // fetch(
+    //     `https://gmail.googleapis.com/gmail/v1/users/shikhatesting0@gmail.com/threads`,
+    //     init
+    // )
+    //     .then((response) => response.json())
+    //     .then(function (data) {
+    //         let userThreadData = data
+    //         // console.log("userThreadData::",userThreadData);
+    //         console.log("thread Ids::",  document.getElementById('threadIds'))
+    //         const myList = document.getElementById('threadIds');
+    //         let newArray = userThreadData.threads;
+    //         // console.log("newArray", newArray);
+    //         newArray.forEach(element => {
+    //         const listItem = document.createElement('li');
+    //         // listItem.innerHTML = `Id: ${element.id} , Snippet: ${element.snippet}`;
+    //         listItem.innerHTML = `Snippet: ${element.snippet}`;
+    //         myList.appendChild(listItem);
+    //        });
+    //     })
+    //     .catch(function (error) {
+    //         console.error('API request error:', error);
+    //     });
 }
 
 async function getThread() {
     console.log('Get thread clicked');
-    const firstThreadElement = document.querySelector('.bqe');
-    if (firstThreadElement) {
-        threadId = firstThreadElement.getAttribute('data-legacy-thread-id');
-        messageId = firstThreadElement.getAttribute('data-legacy-last-message-id');
-        console.log("Thread ID: ", threadId);
-        console.log("Message ID: ", messageId);
+    const getAllThreads =  document.querySelectorAll('.xY.a4W .bog span');
+    let thread=[];
+    let message = [];
+    getAllThreads.forEach(element=>{
+     thread.push(element.getAttribute('data-legacy-thread-id'));
+     message.push(element.getAttribute('data-legacy-last-message-id'));
+        // makeApiRequest(thread);
+    })
+    console.log("thread form js :::", thread);
+    console.log("message form js :::", message);
+    
+    // const firstThreadElement = document.querySelector('.bqe');
+    // if (firstThreadElement) {
+    //     threadId = firstThreadElement.getAttribute('data-legacy-thread-id');
+    //     messageId = firstThreadElement.getAttribute('data-legacy-last-message-id');
+    //     console.log("Thread ID: ", threadId);
+    //     console.log("Message ID: ", messageId);
 
-        // Now that you have the threadId, you can proceed with the API request
-        makeApiRequest(threadId);
-    } else {
-        console.error('Thread element not found');
-    }
+    //     // Now that you have the threadId, you can proceed with the API request
+    //     makeApiRequest(threadId);
+    // } else {
+    //     console.error('Thread element not found');
+    // }
 }
+
